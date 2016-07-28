e31f330cf168cfc8852d1b8a85f2076da86b337e
maleiming <maleimings@gmail.com>
2016-07-28 17:27:50 +0800

    property animation xml

--- LearnAnim/app/src/main/java/com/example/demo/learnanim/MainActivity.java
+++ LearnAnim/app/src/main/java/com/example/demo/learnanim/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.demo.learnanim;
 
-import android.support.v7.app.AppCompatActivity;
+import android.animation.Animator;
+import android.animation.AnimatorInflater;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.text)
+    protected ImageView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        changeText();
+    }
+
+    private void changeText() {
+        Animator animator = AnimatorInflater.loadAnimator(this, R.anim.property_animator);
+        animator.setTarget(mTextView);
+        animator.start();
     }
 }
